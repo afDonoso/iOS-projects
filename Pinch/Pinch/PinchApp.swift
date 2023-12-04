88e439128b4c48b828809a94741ed4ec0768fa2f
Andres Donoso <andy.donoso1@gmail.com>
2023-12-04 18:00:05 -0500

    Finished Restart app

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Andrés Donoso on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pinch/Pinch/Screens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Andrés Donoso on 3/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
